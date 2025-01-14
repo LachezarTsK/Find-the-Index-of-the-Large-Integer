d289416f8b40e8fd004a86734494ff8cb3f8ad2d
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-01-14 17:53:41 +0100

    Create Solution.go

---
+++ Solution.go
@@ -0,0 +1,62 @@
+
+package main
+
+func getIndex(reader *ArrayReader) int {
+    length := reader.length()
+
+    firstLeft := 0
+    firstRight := (length - 1) / 2
+
+    secondLeft := firstRight + balanceIndexForRangeWithOddLength(length)
+    secondRight := length - 1
+
+    var compare = reader.compareSub(firstLeft, firstRight, secondLeft, secondRight)
+
+    for compare != 0 {
+
+        if compare < 0 {
+            length = secondRight - secondLeft + 1
+            firstLeft = secondLeft
+            firstRight = secondLeft + (secondRight - secondLeft)/2
+            secondLeft = firstRight + balanceIndexForRangeWithOddLength(length)
+        } else {
+            length = firstRight - firstLeft + 1
+            secondRight = firstRight
+            firstRight = firstLeft + (firstRight - firstLeft)/2
+            secondLeft = firstRight + balanceIndexForRangeWithOddLength(length)
+        }
+        compare = reader.compareSub(firstLeft, firstRight, secondLeft, secondRight)
+    }
+
+    return firstRight
+}
+
+/*
+When the range that is divided between the two subranges has an odd length,
+then the end of the first range has to overlap with the start of the second range.
+That is to say, if we have subrange(firstLeft, firstRight) and subrange(secondLeft, secondRight),
+then 'firstRight' ==  'secondLeft'.
+*/
+func balanceIndexForRangeWithOddLength(length int) int {
+    return (length % 2) ^ 1
+}
+
+/*
+Interface 'ArrayReader' is implemented internally at leetcode.com.
+When running the code on the website, do not include this interface.
+The comments below are from leetcode.com.
+*/
+
+// This is the ArrayReader's API interface.
+// You should not implement it, or speculate about its implementation
+type ArrayReader struct {
+}
+
+// Compares the sum of arr[l..r] with the sum of arr[x..y]
+// return 1 if sum(arr[l..r]) > sum(arr[x..y])
+// return 0 if sum(arr[l..r]) == sum(arr[x..y])
+// return -1 if sum(arr[l..r]) < sum(arr[x..y])
+func (this *ArrayReader) compareSub(l, r, x, y int) int {}
+
+// Returns the length of the array
+func (this *ArrayReader) length() int {}
