646761e1eac6541b0bdd8b6a9af88262d4505603
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-01-14 17:51:09 +0100

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,59 @@
+
+function getIndex(reader: ArrayReader): number {
+    let length = reader.length();
+
+    let firstLeft = 0;
+    let firstRight = Math.floor((length - 1) / 2);
+
+    let secondLeft = firstRight + balanceIndexForRangeWithOddLength(length);
+    let secondRight = length - 1;
+
+    let compare = reader.compareSub(firstLeft, firstRight, secondLeft, secondRight);
+
+    while (compare !== 0) {
+
+        if (compare < 0) {
+            length = secondRight - secondLeft + 1;
+            firstLeft = secondLeft;
+            firstRight = secondLeft + Math.floor((secondRight - secondLeft) / 2);
+            secondLeft = firstRight + balanceIndexForRangeWithOddLength(length);
+        } else {
+            length = firstRight - firstLeft + 1;
+            secondRight = firstRight;
+            firstRight = firstLeft + Math.floor((firstRight - firstLeft) / 2);
+            secondLeft = firstRight + balanceIndexForRangeWithOddLength(length);
+        }
+        compare = reader.compareSub(firstLeft, firstRight, secondLeft, secondRight);
+    }
+
+    return firstRight;
+};
+
+/*
+ When the range that is divided between the two subranges has an odd length, 
+ then the end of the first range has to overlap with the start of the second range.
+ That is to say, if we have subrange(firstLeft, firstRight) and subrange(secondLeft, secondRight), 
+ then 'firstRight' ==  'secondLeft'.
+ */
+function balanceIndexForRangeWithOddLength(length: number): number {
+    return (length % 2) ^ 1;
+}
+
+/*
+ Interface 'ArrayReader' is implemented internally at leetcode.com.
+ When running the code on the website, do not include this interface.
+ The comments below are from leetcode.com.
+ */
+
+// This is the ArrayReader's API interface.
+// You should not implement it, or speculate about its implementation
+class ArrayReader {
+    // Compares the sum of arr[l..r] with the sum of arr[x..y] 
+    // return 1 if sum(arr[l..r]) > sum(arr[x..y])
+    // return 0 if sum(arr[l..r]) == sum(arr[x..y])
+    // return -1 if sum(arr[l..r]) < sum(arr[x..y])
+    compareSub(l: number, r: number, x: number, y: number): number { };
+
+    // Returns the length of the array
+    length(): number { };
+};
